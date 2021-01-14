7b3bad6797d8380358651ee5c2f1110ebe1576fb
OleStan <59766354+OleStan@users.noreply.github.com>
2021-01-15 00:33:16 +0200

    fix gemfile for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -12,12 +14,11 @@ Category.create([{ name: 'Computer' },
                  { name: 'Computer_child', ancestry: '1' }])
 25.times do
   Product.create(name: Faker::Food.dish,
-                     price: rand(0.1..500.00),
-                     about: Faker::Food.description,
-                     config:  Faker::Food.ingredient,
-                     photo: "https://i2.rozetka.ua/goods/1157159/hlebobulochnie-izdeliya_top_1157159589.jpg",
-                     article: rand(100000..999999),
-                     factory: "111",
-                     category_id: '3',
-                    )
+                 price: rand(0.1..500.00),
+                 about: Faker::Food.description,
+                 config:Faker::Food.ingredient + Faker::Food.ingredient + Faker::Food.ingredient,
+                 photo: 'https://i2.rozetka.ua/goods/1157159/hlebobulochnie-izdeliya_top_1157159589.jpg',
+                 article: rand(100_000..999_999),
+                 factory: '111',
+                 category_id: '3')
 end
