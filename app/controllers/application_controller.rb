86a57c5420460c0b1ecd1ec4b059b6460b4e5ad3
OleStan <59766354+OleStan@users.noreply.github.com>
2021-01-15 00:14:12 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :all_product
-
   include Pagy::Backend
-
-  private
-  def all_product
-    @productsfor = Product.new
-  end
 end
