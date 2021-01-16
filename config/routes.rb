4bea48948c18c61ac37aca5e4f9588be624472a8
OleStan <59766354+OleStan@users.noreply.github.com>
2021-01-16 23:23:09 +0200

    no css on log/sing_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users#, ActiveAdmin::Devise.config
+  devise_for :admin_users#, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'pages#index', as: 'home'
   # get 'category', to: 'pages#category', as: 'category'
