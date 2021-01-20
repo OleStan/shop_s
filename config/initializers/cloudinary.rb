643a210678cb316614befb5f26967cc2a3c2c6fe
OleStan <59766354+OleStan@users.noreply.github.com>
2021-01-20 23:30:40 +0200

    some fix for master branch redy remote storage?

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = "crep"
-  config.api_key = "729731673242531"
-  config.api_secret = "A9-1OAXwa3xKJGtfx3XDyu1eEjk"
-  config.secure = true
-  config.cdn_subdomain = true
-end
